8a891be90ecd88ec4a9fd7db60242a4793b10e1f
mate852 <matefeher852@gmail.com>
2026-03-15 21:23:12 +0100

    Feltöltéshez bugfix 5

--- server/app.js
+++ server/app.js
@@ -22,7 +22,7 @@ app.use("/api/reviews", reviewRoutes);
 
 app.use(express.static(path.join(__dirname, "../client/dist")));
 
-app.get("*", (req, res) => {
+app.get(/.*/, (req, res) => {
   res.sendFile(path.join(__dirname, "../client/dist/index.html"));
 });
 
